a9100262bd12c1823574b0af9792915b557a791a
shahshank <74662711+ali-shahshank@users.noreply.github.com>
2026-05-26 10:59:24 +0000

    MAJOR -/ Project Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import './globals.css';
+import Nav from './components/nav';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Nav />
+      <Box sx={{ m: 0, p: 4, bgcolor: '#f7f7f7' }}>
+        <Typography
+          variant="h2"
+          sx={{ textAlign: 'center', fontWeight: '400' }}
+        >
+          The Ultamate AI Productivity Platform
+        </Typography>
+        <Typography variant="h3">Subheading Text</Typography>
+      </Box>
+    </>
+  );
 };
 
 export default page;
